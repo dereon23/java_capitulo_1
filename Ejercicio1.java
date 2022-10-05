bfa8597ae2e9157bd3cb5b72fe1c2e899db4188b
dereon23 <115071403+dereon23@users.noreply.github.com>
2022-10-05 12:44:51 +0200

    Add files via upload
    Execise one

---
+++ Ejercicio1.java
@@ -0,0 +1,30 @@
+
+package ejercBucles;
+
+import java.util.*;
+
+public class Ejercicio1 {
+	
+	public static void main(String[] args) {
+
+
+	String nombre;
+	char sexo;
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("Introduzca su nombre completo");
+	nombre = scanner.nextLine();
+	
+	System.out.println("Introduzca su sexo(M/F)");
+	sexo = scanner.next().charAt(0);
+	
+	if(sexo=='M'){
+		System.out.println("Bienvenido, Sr. " + nombre);
+	}else if(sexo == 'F') {
+		System.out.println("Bienvenido, Sra. " + nombre);
+	}
+
+    
+	}
+	
+}
